b98053b2a0bee8762fb224e02741ae0f86100e6d
Sugarkhuu <sugarkhuul@gmail.com>
2018-10-06 22:06:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/sugar2/firstapplication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.sugar2.firstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
